4186d16116d824b0779ca124f95af88cfc3f6754
EvgeniNS <89783197+EvgeniNS@users.noreply.github.com>
2022-09-04 01:05:32 +0300

    update

--- strings2.py
+++ strings2.py
@@ -25,7 +25,7 @@ print('----')
 print(parrot[ : ])
 
 print('----------')
-#          01234567890123456789012345---25 -26
+#          01234567890123456789012345---25 -26   "Hi My name is evgeni and im learning strings"
 letters = "abcdefghijklmnopqrstuvwxyz"
 
 print(letters[25])
@@ -45,4 +45,12 @@ print(letters[-22] + letters[-5] + letters[-20] + letters[-22] + letters[-13] + 
 
 print('----')
 
-print(letters[4:5] + letters[21:22] + letters[6:7] + letters[4:5] + letters[13:14] + letters[8:9])
+print(letters[-22:-21] + letters[-5:-4] + letters[-20:-19] + letters[-22:-21] + letters[-13:-12] + letters[-18:-17])    #becuse im using the -26 letters its work like when [25:26] its from 25 to 26 but not including the the 26 the same works to [-22:-21] its from -22 to -21 but not including -21.
+
+print('----')
+
+#      012345678901234567890123456==26 -27   "Hi My name is evgeni and im learning strings"
+kek = " abcdefghijklmnopqrstuvwxyz"
+
+print(kek[8] + kek[9] + kek[0] + kek[13] + kek[-2] + kek[0] + kek[14] + kek[1] + kek[13] + kek[5] + kek[0] + kek[9] + kek[-8] + kek[0] + kek[5] + kek[22] + kek[7] + kek[5] + kek[14] + kek[9] +
+      kek[0] + kek[1] + kek[14] + kek[4] + kek[0] + kek[9] + kek[13] + kek[0] + kek[12] + kek[5] + kek[1] + kek[-9] + kek[14] + kek[9] + kek[14] + kek[7] + kek[0] + kek[-8] + kek[-7] + kek[-9] + kek[9] + kek[14] + kek[7] + kek[-8])
