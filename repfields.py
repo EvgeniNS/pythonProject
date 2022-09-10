bb599b872facc73e3ef41b525c9a4e3cbfcb881f
EvgeniNS <89783197+EvgeniNS@users.noreply.github.com>
2022-09-10 18:45:23 +0300

    update

---
+++ repfields.py
@@ -0,0 +1,24 @@
+age = "24"
+print("my age is " + str(age) + " years")
+print("my age is {0} years".format(age))
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
+      .format(31,"jan","mar","may","jul","aug","oct","dec"))
+print("There are {0} days in jan, mar, may, jul, aug, oct and dec".format(31))
+
+print("jan: {2}, feb: {0}, mar: {2}, apr: {1}, may: {2}, jun: {1}, jul: {2}, sep: {1}, oct: {2}, nov: {1}, dec: {2}"
+      .format(28,30,31))
+
+print()
+
+print("""jan: {2}
+feb: {0}
+mar: {2}
+apr: {1}
+may: {2}
+jun: {1}
+jul: {2}
+sep: {1}
+oct: {2}
+nov: {1}
+dec: {2}"""
+      .format(28,30,31))
